01bfa33eaf78042ecc68b520b327125e13a21e8b
alvaroca05 <alvarocab0510@gmail.com>
2026-02-19 11:37:11 +0100

    Factoria para llevar

--- Practica1/Abstract_Factory/FactoriaParaLlevar.java
+++ Practica1/Abstract_Factory/FactoriaParaLlevar.java
@@ -1,5 +1,32 @@
 package Practica1.Abstract_Factory;
 
-public class FactoriaParaLlevar {
-    
+// Implementamos la interfaz que define las reglas de las factorías
+public class FactoriaParaLlevar implements FactoriaAbstracta {
+
+    @Override
+    public Menu crearMenuSemanal(String tipoAcompanamiento) {
+        // Le pasamos un 'true' al menú para avisarle de que es un pedido para llevar
+        // Así, cuando el menú calcule su precio final, sabrá que tiene que sumar el 2%
+        MenuSemanal menu = new MenuSemanal(true);
+
+        // Regla del local: El menú semanal para llevar SOLO tiene entrante y principal
+        menu.asignarPlato(new Plato("Sopa de fideos", 5.00, "Entrante", "Ninguno"));
+        menu.asignarPlato(new Plato("Pollo asado", 10.00, "Principal", tipoAcompanamiento));
+
+        // ¡Ojo! No le metemos postre.
+        return menu;
+    }
+
+    @Override
+    public Menu crearMenuTemporada(String tipoAcompanamiento) {
+        // Igual que antes, le decimos que es para llevar (true)
+        MenuTemporada menu = new MenuTemporada(true);
+
+        // En el de temporada vamos a suponer que sí les damos el postre
+        menu.asignarPlato(new Plato("Crema de calabaza", 6.00, "Entrante", "Ninguno"));
+        menu.asignarPlato(new Plato("Costillas BBQ", 14.00, "Principal", tipoAcompanamiento));
+        menu.asignarPlato(new Plato("Tarta de queso", 4.50, "Postre", "Ninguno"));
+
+        return menu;
+    }
 }
