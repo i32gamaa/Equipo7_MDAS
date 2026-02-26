6e44266318db126ab11b1bbe969e2cd44c67c8ad
i32hamaj <i32hamaj@uco.es>
2026-02-26 11:11:38 +0100

    fix factoria abstracta

--- Practica1/Abstract_Factory/FactoriaAbstracta.java
+++ Practica1/Abstract_Factory/FactoriaAbstracta.java
@@ -3,9 +3,9 @@ package Practica1.Abstract_Factory;
 public interface FactoriaAbstracta {
 
     // Método para fabricar un Menú Semanal que recibe como parámetro el tipo de acompañamiento (ensalada o patatas)
-    MenuSemanal crearMenuSemanal(Plato.TipoAcompanamiento acompañamiento);
+    Menu crearMenuSemanal(Plato.TipoAcompanamiento acompañamiento);
     
     // Método para fabricar un Menú de Temporada
-    MenuTemporada crearMenuTemporada();
+    Menu crearMenuTemporada();
 
 }
