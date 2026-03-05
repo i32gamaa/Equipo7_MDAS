5f43b709cb0ea1b99632712a88d054906738dd1c
i32hamaj <i32hamaj@uco.es>
2026-03-05 10:16:51 +0100

    Maquetador Avanzado

--- Practica1/Adapter/MaquetadorAvanzado.java
+++ Practica1/Adapter/MaquetadorAvanzado.java
@@ -1,1 +1,14 @@
-.
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+public interface MaquetadorAvanzado {
+    // 1) Unir dos ficheros de texto
+    void unirFicheros(File f1, File f2, File destino) throws IOException;
+
+    // 2) Combinar contenido intercalando párrafos
+    void combinarFicheros(File f1, File f2, List<int[]> parrafosF1, List<int[]> parrafosF2, File destino) throws IOException;
+
+    // 3) Separar fichero en varios indicando puntos de corte
+    void separarFichero(File archivoOriginal, List<Integer> puntosCorte, List<File> archivosDestino) throws IOException;
+}
